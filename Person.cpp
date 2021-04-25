2a7381fae267a0f3ecd6cf15533c0692616f1b18
Sean <82683346+SResor@users.noreply.github.com>
2021-04-24 17:54:54 -0700

    Midterm Code Submission
    Updated with code submitted for the Midterm.

--- Person.cpp
+++ Person.cpp
@@ -10,29 +10,70 @@
 #include <iostream>
 #include "Person.h"
 
+// Default constructor for the Person class
 person::person()
-{}
+{
+	firstLength = 0;
+	lastLength = 0;
+	firstName = new char[1] { '\0' };
+	lastName = new char[1] { '\0' };
+}
 
 // Constructor for the Person class
 person::person(const char* fN, const char* lN)
 {
-	firstName = new char[sizeof fN + 1];
-	strcpy(firstName, fN);
-	lastName = new char[sizeof lN + 1];
-	strcpy(lastName, lN);
+	// Sets the variables for the length of the first and last name
+	firstLength = 0;
+	lastLength = 0;
+
+	// Increments the variable for the length of the first name based on passed char pointer
+	while (fN[firstLength] != '\0')
+	{
+		firstLength++;
+	}
+
+	// Increments the variable for the length of the last name based on passed char pointer
+	while (lN[lastLength] != '\0')
+	{
+		lastLength++;
+	}
+
+	// Assigns a new pointer variable for the first name
+	firstName = new char[firstLength + 1];
+
+	// Copies the values of the passed char pointer into the new variable
+	for (int i = 0; i < firstLength; i++)
+	{
+		firstName[i] = fN[i];
+	}
+
+	// Adds a null character onto the end of the array
+	firstName[firstLength] = '\0';
+
+	// Assigns a new pointer variable for the last name
+	lastName = new char[lastLength + 1];
+
+	// Copies the values of the passed char pointer into the new variable
+	for (int i = 0; i < lastLength; i++)
+	{
+		lastName[i] = lN[i];
+	}
+
+	// Adds a null character onto the end of the array
+	lastName[lastLength] = '\0';
 }
 
 // Destructor for the Person class
 person::~person()
 {
-	delete firstName;
-	delete lastName;
+	delete[] firstName;
+	delete[] lastName;
 }
 
 // Show function to print the first and last names
 void person::Show()
 {
-	cout << "The person's name is:" << endl;
+	cout << "The person's name is: ";
 	cout << firstName << " " << lastName << endl << endl;
 }
 
@@ -51,10 +92,54 @@ char* person::getLastName()
 // Operator overload for the assignment operator
 person& person::operator=(person const& x)
 {
-	firstName = new char[sizeof x.firstName];
-	strcpy(firstName, x.firstName);
-	lastName = new char[sizeof x.lastName];
-	strcpy(lastName, x.lastName);
+	// Deletes the pointer variables if they exist
+	delete[] firstName;
+	delete[] lastName;
+
+	firstLength = x.firstLength;
+	lastLength = x.lastLength;
+
+	// Assigns a new pointer variable for the first name
+	firstName = new char[x.firstLength];
+
+	// Copies the letters for the first name from the parameter object
+	for (int i = 0; i < x.firstLength; i++)
+	{
+		firstName[i] = x.firstName[i];
+	}
+
+	// Assigns a new pointer variable for the last name
+	lastName = new char[x.lastLength];
+
+	// Copies the letters for the last name from the parameter object
+	for (int i = 0; i < x.lastLength; i++)
+	{
+		lastName[i] = x.lastName[i];
+	}
 
 	return *this;
+}
+
+person::person(const person& x)
+{
+	firstLength = x.firstLength;
+	lastLength = x.lastLength;
+
+	// Assigns a new pointer variable for the first name
+	firstName = new char[x.firstLength];
+
+	// Copies the letters for the first name from the parameter object
+	for (int i = 0; i < x.firstLength; i++)
+	{
+		firstName[i] = x.firstName[i];
+	}
+
+	// Assigns a new pointer variable for the last name
+	lastName = new char[x.lastLength];
+
+	// Copies the letters for the last name from the parameter object
+	for (int i = 0; i < x.lastLength; i++)
+	{
+		lastName[i] = x.lastName[i];
+	}
 }
