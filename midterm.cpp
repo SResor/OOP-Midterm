27f54a5a66c2aac767e0af15d26c9bcf0592a281
Sean <82683346+SResor@users.noreply.github.com>
2021-04-24 17:56:18 -0700

    Midterm Code Submission
    Updated with code submitted for the Midterm.

--- midterm.cpp
+++ midterm.cpp
@@ -8,6 +8,7 @@
 #include <stdio.h>
 #include <iostream>
 #include <string>
+#include <ctime>
 #include "BadDude.h"
 
 // Using standard namespace
@@ -15,55 +16,88 @@ using namespace std;
 
 int main()
 {
-	// Sets the name variables to the first values
-	const char* first = "Sally";
-	const char* last = "Smith";
+	// Sets the seed of the random number generator
+	srand(time(0));
+
 
 	// Instantiates a Person object
-	person Sally(first, last);
+	person* Sally = new person("Sally", "Smith");
 
 	// Prints the Person object's name
-	Sally.Show();
+	Sally->Show();
 
-	// Resets the name variables
-	first = "Wyatt";
-	last = "Earp";
+	// Instantiates a Person object
+	person* Steve = new person();
+
+	// Uses overloaded assignment operator to set new Person object equal to old Person object
+	Steve = Sally;
+
+	cout << "Demonstrating successful assignment...\n";
+
+	// Prints the Person object's name
+	Steve->Show();
+
+	// Calls destructor for Person objects
+	delete Sally;
+
 
 	// Instantiates a Gunslinger object
-	gunslinger Wyatt(first, last, 1.5, 10);
+	gunslinger* Wyatt = new gunslinger("Wyatt", "Earp", 1.5, 10);
 	
 	// Prints the Gunslinger object's details
-	Wyatt.Show();
+	Wyatt->Show();
 
 	// Prints the Gunslinger object's draw speed
-	cout << "Their draw speed is: " << Wyatt.Draw() << endl << endl;
+	cout << "Their draw speed is: " << Wyatt->Draw() << endl << endl;
 
-	// Resets the name variables
-	first = "Victoria";
-	last = "Coren";
+	cout << "Demonstrating successful assignment...\n";
+
+	// The next three lines demonstrate the functioning assignment operator
+	gunslinger* Doc = new gunslinger();
+	Doc = Wyatt;
+	Doc->Show();
+
+	// Calls destructor for Gunslinger objects
+	delete Wyatt;
 
 	// Instantiates a PokerPlayer object
-	pokerPlayer Victoria(first, last);
+	pokerPlayer* Victoria = new pokerPlayer("Victoria", "Coren");
 
 	// Prints the PokerPlayer object's details
-	Victoria.Show();
+	Victoria->Show();
 
 	// Draws a card for the PokerPlayer object
-	cout << "The card drawn is card number: " << Victoria.Draw() << endl << endl;
+	cout << "The card drawn is card number: " << Victoria->Draw() << endl << endl;
 
-	// Resets the name variables
-	first = "Michael";
-	last = "Scott";
+	cout << "Demonstrating successful assignment...\n";
+
+	// The next three lines demonstrate the functioning assignment operator
+	pokerPlayer* Daniel = new pokerPlayer();
+	Daniel = Victoria;
+	Daniel->Show();
+
+	// Calls destructor for PokerPlayer objects
+	delete Victoria;
 
 	// Instantiates a BadDude object
-	badDude Michael(first, last, 3.5, 1);
+	badDude* Michael = new badDude("Michael", "Scott", 3.5, 1);
 
 	// Prints the BadDude object's details
-	Michael.Show();
+	Michael->Show();
 
 	// Prints out the BadDude object's draw speed and draws a card for them
-	cout << "Their draw speed is: " << Michael.Gdraw() << endl;
-	cout << "The card they draw is card number: " << Michael.Cdraw() << endl << endl;
+	cout << "Their draw speed is: " << Michael->Gdraw() << endl;
+	cout << "The card they draw is card number: " << Michael->Cdraw() << endl << endl;
+
+	cout << "Demonstrating successful assignment...\n";
+
+	// The next three lines demonstrate the functioning assignment operator
+	badDude* Dwight = new badDude();
+	Dwight = Michael;
+	Dwight->Show();
+
+	// Calls destructor for BadDude objects
+	delete Michael;
 
 	return 0;
 }
