b944b2b7544e3db491bdf3decafad97390a88b86
Sean <82683346+SResor@users.noreply.github.com>
2021-04-24 17:52:10 -0700

    Midterm Submission Code
    Updated with code submitted for midterm

--- BadDude.cpp
+++ BadDude.cpp
@@ -1,7 +1,7 @@
 /*
-    @file BadDude.cpp
-    @author Sean Resor
-    @date 4/12/21
+	@file BadDude.cpp
+	@author Sean Resor
+	@date 4/12/21
  */
 
 #pragma once
@@ -9,36 +9,39 @@
 #include <iostream>
 #include "BadDude.h"
 
-    // Constructor for BadDude class
-    badDude::badDude(const char* fN, const char* lN, double d, int n): gunslinger(fN, lN, d, n)
-    {
-    }
+badDude::badDude() : gunslinger()
+{}
 
-    // Function to return the Gunslinger class's Draw() function
-    double badDude::Gdraw()
-    {
-        return gunslinger::Draw();
-    }
+ // Constructor for BadDude class
+badDude::badDude(const char* fN, const char* lN, double d, int n) : gunslinger(fN, lN, d, n), person(fN, lN)
+{
+}
 
-    // Function to return the PokerPlayer class's Draw() function
-    int badDude::Cdraw()
-    {
-        return pokerPlayer::Draw();
-    }
+// Function to return the Gunslinger class's Draw() function
+double badDude::Gdraw()
+{
+	return gunslinger::Draw();
+}
 
-    // Function to print the first and last names, the draw speed, and the number of notches
-    void badDude::Show()
-    {
-        cout << "The Bad Dude is: " << endl;
-        cout << gunslinger::getFirstName() << " " << gunslinger::getLastName() << endl;
-        cout << "Draw speed: " << getDraw() << endl;
-        cout << "Gun notches: " << getNotches() << endl << endl;
-    }
+// Function to return the PokerPlayer class's Draw() function
+int badDude::Cdraw()
+{
+	return pokerPlayer::Draw();
+}
 
-    // Operator overload for the assignment operator
-    badDude& badDude::operator=(badDude const& x)
-    {
-        gunslinger::operator=(x);
+// Function to print the first and last names, the draw speed, and the number of notches
+void badDude::Show()
+{
+	cout << "The Bad Dude is: ";
+	cout << person::getFirstName() << " " << person::getLastName() << endl;
+	cout << "Draw speed: " << getDraw() << endl;
+	cout << "Gun notches: " << getNotches() << endl << endl;
+}
 
-        return *this;
-    }
+// Operator overload for the assignment operator
+badDude& badDude::operator=(badDude const& x)
+{
+	gunslinger::operator=(x);
+
+	return *this;
+}
